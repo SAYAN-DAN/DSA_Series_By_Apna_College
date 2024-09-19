743104738a10ad1583c3f16534ea5535e20a9ef0
SAYAN-DAN <sayan94.arambagh@gmail.com>
2024-09-19 23:11:55 +0530

    Commit

--- Linear_Search.cpp
+++ Linear_Search.cpp
@@ -1,28 +1,31 @@
+// Linear_Search.cpp
 #include <iostream>
 using namespace std;
+
+int Linear_Search(int arr[], int size, int target)
+{
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] == target)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
 
 int main()
 {
     int size;
     cout << "Enter the number of elements in the array: " << endl;
     cin >> size;
-
+    int target = 12;
     int arr[size]; // Declare array after getting size
     cout << "Enter the all element:" << endl;
     for (int i = 0; i < size; i++)
     {
         cin >> arr[i];
     }
-    int minterm = arr[0];
-    int index = 0;
-    for (int i = 1; i < size; i++)
-    {
-        if (minterm > arr[i])
-        {
-            minterm = arr[i];
-            index = i;
-        }
-    }
-    cout << "The minterm is : " << minterm << " and the index of the minterm is: " << index;
+    cout << Linear_Search(arr, size, target) << endl;
     return 0;
 }
