d0b477d75fb12a4e9bdc12d261d17c118a6dce0a
SAYAN-DAN <sayan94.arambagh@gmail.com>
2024-09-20 08:02:37 +0530

    Solve ones more

--- Array.cpp
+++ Array.cpp
@@ -82,20 +82,68 @@ int unique_number(int arr[], int size)
         }
     }
 }
+
+// int main()
+// {
+//     int size;
+//     cout << "Enter the number of Array: " << endl;
+//     cin >> size;
+//     int arr[size];
+//     cout << "Enter the Arrays: " << endl;
+//     for (int i = 0; i < size; i++)
+//     {
+//         cin >> arr[i];
+//     }
+//     // reverse_array_1(arr, size);
+//     // reverse_Array_2(arr, size);
+//     // sum_product_array(arr, size);
+//     // swap_max_min_number(arr, size);
+//     // unique_number(arr, size);
+// }
+
+
+
+
+// problem no -5
+// WAP TO PRINT THE INTERSECTION OF TWO ARRAYS
+
+int intersection(int arr1[], int arr2[], int size1, int size2)
+{
+    for (int i = 0; i < size1; i++)
+    {
+        for (int j = 0; j < size2; j++)
+        {
+            if (arr1[i] == arr2[j])
+            {
+                cout <<"The intersection elemenrs are: "<< arr1[i] << " ";
+            }
+        }
+            cout << endl;
+    }
+}
 int main()
 {
-    int size;
-    cout << "Enter the number of Array: " << endl;
-    cin >> size;
-    int arr[size];
-    cout << "Enter the Arrays: " << endl;
-    for (int i = 0; i < size; i++)
+    cout << "Enter the number of element of the firrst Araay: ";
+    int size1;
+    cin >> size1;
+    int arr1[size1];
+    cout << "Enter the 1st Array elements: ";
+    for (int i = 0; i < size1; i++)
     {
-        cin >> arr[i];
+        cin >> arr1[i];
     }
-    // reverse_array_1(arr, size);
-    // reverse_Array_2(arr, size);
-    // sum_product_array(arr, size);
-    // swap_max_min_number(arr, size);
-    // unique_number(arr, size);
+
+    cout << "Enter the number of element of the firrst Araay: ";
+    int size2;
+    cin >> size2;
+    int arr2[size2];
+    cout << "Enter the 1st Array elements: ";
+    for (int i = 0; i < size2; i++)
+    {
+        cin >> arr2[i];
+    }
+
+    intersection(arr1, arr2, size1, size2);
 }
+
+
